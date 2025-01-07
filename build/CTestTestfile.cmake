0ccccd8288a219d75e41eacbbb4f987bf79b14df
BryanFabriceMoreira0 <bryanfabricemoreira@gmail.com>
2025-01-07 23:15:41 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bryanmoreira/Code/C++/appointment_project
-# Build directory: /Users/bryanmoreira/Code/C++/appointment_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
