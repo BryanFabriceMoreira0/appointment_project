15d0833c4a8b13035a80c2b1e79bf29d6eba0b8e
BryanFabriceMoreira0 <bryanfabricemoreira@gmail.com>
2025-01-07 23:00:13 +0100

    Initial commit: Project structure with directories and basic files

---
+++ build/CMakeFiles/appointment_project.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/appointment_project.dir/main.cpp.o: \
+  /Users/bryanmoreira/Code/C++/appointment_project/main.cpp
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bryanmoreira/Code/C++/appointment_project
+# Build directory: /Users/bryanmoreira/Code/C++/appointment_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
